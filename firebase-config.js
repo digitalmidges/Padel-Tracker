21394bcdf7f4dbd36c4855b3b4b211b9127c472d
Oded F <digitalmidges@gmail.com>
2026-06-11 18:59:08 +0300

    Connect app to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBv9xibtn1Wi2JOxF66EHYfno5WwL5sXUk",
+  authDomain: "padel-tracker-574c2.firebaseapp.com",
+  projectId: "padel-tracker-574c2",
+  storageBucket: "padel-tracker-574c2.firebasestorage.app",
+  messagingSenderId: "466122648941",
+  appId: "1:466122648941:web:ea2dfd88952f2abd773e64",
+  measurementId: "G-6067TB0MB3"
 };
 
 export const firebaseOptions = {
-  enabled: false,
+  enabled: true,
   tournamentId: "main"
 };
